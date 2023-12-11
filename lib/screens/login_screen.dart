1f0b58289a87654267cc17173661140e877b515d
Eusuf Uddin <eusuf15-8926@diu.edu.bd>
2023-12-11 10:51:12 +0600

    login ui init

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+class Login_Screen extends StatefulWidget {
+  const Login_Screen({super.key});
+
+  @override
+  State<Login_Screen> createState() => _Lofin_ScreenState();
+}
+
+class _Lofin_ScreenState extends State<Login_Screen> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          width: double.infinity,
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: NetworkImage(
+                      "https://e0.pxfuel.com/wallpapers/105/975/desktop-wallpaper-blue-background-lock-screen-samsung-galaxy-s5-clothes-color-iphone-s5-blue-background-light-blue-plain.jpg"))),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+                child: CircleAvatar(
+                  radius: 22,
+                  child: Icon(Icons.arrow_back_ios_new, color: Colors.black87),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+                child: Text(
+                  "Welcome \nBack",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 30,
+                      fontWeight: FontWeight.w700),
+                ),
+              ),
+              Spacer(),
+              Container(
+                width: double.infinity,
+                height: 600,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(topLeft: Radius.circular(20), topRight: Radius.circular(20))
+                ),
+                child: Column(
+                  children: [
+                    SizedBox(height: 20,),
+                    Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          prefix: Icon(Icons.email, color: Colors.blue),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20),
+                            borderSide: BorderSide(color: Colors.blue)
+                          )
+
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
